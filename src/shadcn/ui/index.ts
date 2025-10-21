692abf52b72b1b72644073c8e2d59c19f64ef24e
seunjin <jinseun@gmail.com>
2025-10-21 20:07:02 +0900

    feat:  shadcn/ui 배럴 파일 생성

---
+++ src/shadcn/ui/index.ts
@@ -0,0 +1,17 @@
+export { Button, buttonVariants } from "./button";
+export { Input } from "./input";
+export { Label } from "./label";
+export { Separator } from "./separator";
+export { Skeleton } from "./skeleton";
+export {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "./select";
