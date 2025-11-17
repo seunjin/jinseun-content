6989649734914700e33a94178875a5c35c9ecc83
seunjin <jinseun@gmail.com>
2025-11-17 22:19:41 +0900

    fix: 토스트 기능 수정
    - Toaster 위치를 top-center로 변경\n- expand 옵션 활성화로 알림 확장 표시

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           {children}
           <ClientDialogRenderer />
         </NextQueryClientProvider>
-        <Toaster />
+        <Toaster expand position={`top-center`} />
       </body>
     </html>
   );
